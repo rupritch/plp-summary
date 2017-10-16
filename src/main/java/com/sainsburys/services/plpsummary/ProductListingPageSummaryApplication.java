bd16949441e28fd08cce4aa26be872f5fb77f5a2
Russell Pritchard <Russell.Pritchard@waitrose.co.uk>
2017-10-16 08:24:32 +0100

    Write response to plp-summary.json file and improve logging.

--- src/main/java/com/sainsburys/services/plpsummary/ProductListingPageSummaryApplication.java
+++ src/main/java/com/sainsburys/services/plpsummary/ProductListingPageSummaryApplication.java
@@ -5,15 +5,24 @@ import com.google.gson.GsonBuilder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.sainsburys.services.plpsummary.config.ProductListingPageSummaryServiceModule;
+import com.sainsburys.services.plpsummary.reader.ProductListingPageSummaryHtmlReader;
 import com.sainsburys.services.plpsummary.request.ProductListingPageSummaryRequest;
 import com.sainsburys.services.plpsummary.response.ProductListingPageSummaryResponse;
 import com.sainsburys.services.plpsummary.service.ProductListingPageSummaryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+/**
+ * Application that calls the ProductListingPageSummaryService.
+ */
 public class ProductListingPageSummaryApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductListingPageSummaryHtmlReader.class);
 
     private static final String FALLBACK_URL = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
 
@@ -29,13 +38,14 @@ public class ProductListingPageSummaryApplication {
 
         Injector injector = Guice.createInjector(new ProductListingPageSummaryServiceModule());
         ProductListingPageSummaryService productListingPageSummaryService = injector.getInstance(ProductListingPageSummaryService.class);
-        try {
+        try (FileWriter fileWriter = new FileWriter("plp-summary.json")) {
             ProductListingPageSummaryResponse productListingPageSummaryResponse = productListingPageSummaryService.createProductListingPageSummary(productListingPageSummaryRequest);
-            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-            System.out.println(gson.toJson(productListingPageSummaryResponse));
+            Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
+            logger.info("Writing output to plp-summary.json file");
+            fileWriter.write(gson.toJson(productListingPageSummaryResponse));
+            logger.info("PLP summary generated successfully. Please check plp-summary.json for output");
         } catch (Exception e) {
-            //TODO RDP: Improve messaging here to the caller!!!
-            System.out.println("Something went wrong...fix it.");
+            logger.error("Fatal error executing request for PLP summaries. Reason: ", e);
         }
     }
 }
